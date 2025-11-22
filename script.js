2349e324e962cf245e05db264a5769687842aeb3
Rahul Singh <rahulsinghrajpoot9997@gmail.com>
2025-11-22 21:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateTime() {
+	const now = new Date();
+	const date = now.toLocaleDateString("en-US");
+	const time = now.toLocaleTimeString("en-US");
+
+	document.getElementById("timer").textContent=`${date}, ${time}`;
+}
+
+setInterval(updateTime, 1000);
+updateTime()
